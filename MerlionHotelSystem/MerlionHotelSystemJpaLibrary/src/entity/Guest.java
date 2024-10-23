837810d4e83e893342ec9f492a3deff9c728f70b
Pramoth <pramoth@Pramoths-MacBook-Air.local>
2024-10-24 01:43:02 +0800

    Add Room, Customer, Guest, and Partner entity classes

---
+++ MerlionHotelSystem/MerlionHotelSystemJpaLibrary/src/entity/Guest.java
@@ -0,0 +1,115 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author pramoth
+ */
+@Entity
+public class Guest implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long guestId;
+    
+    @Column(nullable = false, length = 50)
+    private String firstName;
+    
+    @Column(nullable = false, length = 50)
+    private String lastName;
+    
+    @Column(nullable = false, length = 15)
+    private String phoneNumber;
+    
+    @Column(nullable = false, length = 20)
+    private String identificationNumber;
+    
+    public Guest() {
+    }
+
+    public Guest(String firstName, String lastName, String phoneNumber, String identificationNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.identificationNumber = identificationNumber;
+    }
+
+    // Getters and Setters
+    public Long getGuestId() {
+        return guestId;
+    }
+
+    public void setGuestId(Long guestId) {
+        this.guestId = guestId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getIdentificationNumber() {
+        return identificationNumber;
+    }
+
+    public void setIdentificationNumber(String identificationNumber) {
+        this.identificationNumber = identificationNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (guestId != null ? guestId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the guestId fields are not set
+        if (!(object instanceof Guest)) {
+            return false;
+        }
+        Guest other = (Guest) object;
+        if ((this.guestId == null && other.guestId != null) || (this.guestId != null && !this.guestId.equals(other.guestId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Guest[ id=" + guestId + " ]";
+    }
+    
+}
